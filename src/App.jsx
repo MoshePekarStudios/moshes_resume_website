3f16332fc386bf432f3acca0a4bc2dece79cd1bc
MosesPe <ipekar@gmail.com>
2022-11-27 19:09:54 -0500

    Added React Terminal

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,23 @@
 import './App.css'
 import Background from './Background'
 import Header from './Header'
-
+import { ReactTerminal } from "react-terminal";
 
 function App() {
-  
+  const commands = {
+    'Moshe.Location':'"Brooklyn, NY"',
+    'Moshe.Skills':'"[HTML ,CSS , JavaScript, ReactJS, NodeJS, W3.css, Bootstrap, MUI ]"'
+  }
 
   return (
    <Background>
      <div className="App">
      
   <Header>Moshe Pekar</Header>
+  <div id='Terminal' style={{width:'50vw', height:'50vh',padding:'10rem',color:'blue'}}>
+  <ReactTerminal commands={commands} prompt={'➡️'} welcomeMessage={<><h2>Hello!</h2><br/><h3>Type <code>Moshe.Location</code> for my Location!<br/>Type <code>Moshe.Skills</code> for my Skills!</h3><hr/></>}/>
+  </div>
+  
     </div>
    </Background>
    
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
+import { TerminalContextProvider } from "react-terminal";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TerminalContextProvider>
+    <App/>
+  </TerminalContextProvider>
   </React.StrictMode>
 )
