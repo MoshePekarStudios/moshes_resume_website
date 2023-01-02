3a83bba8324f3107e222eb93974daec8e4a8a2fc
MosesPe <ipekar@gmail.com>
2023-01-02 18:29:40 -0500

    styled terminal

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,50 @@ import { ReactTerminal } from "react-terminal";
 function App() {
   const commands = {
     'Moshe.Location':'"Brooklyn, NY"',
-    'Moshe.Skills':'"[HTML ,CSS , JavaScript, ReactJS, NodeJS, W3.css, Bootstrap, MUI ]"'
+    'Moshe.Skills':'"[HTML ,CSS , JavaScript, ReactJS, NodeJS, W3.css, Bootstrap, MUI ]"',
+    'Moshe.Projects':<><a href="https://logo-finder-sooty.vercel.app"><h2>Logo Finder</h2></a><br/><a href="#"><h2>Resume website</h2></a></>,
+    
   }
-
-  return (
-   <Background>
-     <div className="App">
-     
-  <Header>Moshe Pekar</Header>
-  <div id='Terminal' style={{width:'50vw', height:'50vh',padding:'10rem',color:'blue'}}>
-  <ReactTerminal commands={commands} prompt={'➡️'} welcomeMessage={<><h2>Hello!</h2><br/><h3>Type <code>Moshe.Location</code> for my Location!<br/>Type <code>Moshe.Skills</code> for my Skills!</h3><hr/></>}/>
-  </div>
+ 
   
-    </div>
-   </Background>
+  return (
+    <Background>
+  <div className="App">
+  <Header>Moshe Pekar</Header>
+  <div className="Terminals">
+
+            <div className='TerminalWrapper' style={{width:'40vw', height:'50vh',margin:'5rem',color:'blue',backdropFilter: 'blur(5px)',overflow:'hidden'}}>
+            <ReactTerminal commands={commands} prompt={'➡️'} welcomeMessage={<><h2>Hello!</h2><br/><h3>Type <code>Moshe.Location</code> for my Location!</h3><h3><br/>Type <code>Moshe.Skills</code> for my Skills!</h3><br /><h3>Type <code>Moshe.Projects</code> for links to my Projects!</h3><hr/></>} themes={{
+              myCustomtheme: {
+                themeBGColor: "transparent",
+                themeToolbarColor: "tranparent",
+                themeColor: "white",
+                themePromptColor: "#a917a8"
+          
+              }
+            }}  theme="myCustomtheme"/>
+          
+              </div>
+              {/* <div className='TerminalWrapper2' style={{width:'40vw', height:'50vh',margin:'5rem',color:'blue',backdropFilter: 'blur(5px)',overflow:'hidden'}}>
+            <ReactTerminal commands={commands} prompt={'➡️'} welcomeMessage={<><h2>Type <span style={{'fontVariant':'all-small-caps','background':'grey'}}></span>get advice</h2><br /><hr/></>} themes={{
+              myCustomtheme: {
+                themeBGColor: "transparent",
+                themeToolbarColor: "tranparent",
+                themeColor: "white",
+                themePromptColor: "#a917a8"
+          
+              }
+            }}  theme="myCustomtheme"/>
+          
+              </div> */}
+  </div>
    
+</div>
+
+  </Background>
+  
+
+
   )
 }
 
